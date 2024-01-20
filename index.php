3907709d8dd7fc7b63dc82b9c5eb10922e57895c
Nicholas Roberts <82165867+trissentety@users.noreply.github.com>
2024-01-20 18:34:11 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Currently viewing Home page<br>
+    Stuff about home page goes here<br>
+</body>
+
+</html>
+<?php
+include("footer.html")
+?>
